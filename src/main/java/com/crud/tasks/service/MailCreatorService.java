9b2537f73ffa803ce3e45c2a829ba58a61b044fa
Agnieszka <agnfal@gmail.com>
2020-12-14 11:08:10 +0100

    zadanie 32.3

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.trello.config.AdminConfig;
 import com.crud.tasks.trello.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class MailCreatorService {
@@ -17,8 +21,15 @@ public class MailCreatorService {
     private AdminConfig adminConfig;
     @Autowired
     private CompanyConfig companyConfig;
+    @Autowired
+    private DbService dbService;
 
     public String buildTrelloCardEmail(String message){
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection whit Trello account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message",message);
         context.setVariable("tasks_url","http://localhost:8888/crud");
@@ -28,6 +39,21 @@ public class MailCreatorService {
         context.setVariable("company_email",companyConfig.getCompanyEmail());
         context.setVariable("company_phone",companyConfig.getCompanyPhone());
         context.setVariable("message_preview", createMessagePreview(message));
+        context.setVariable("show_button",false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config",adminConfig);
+        context.setVariable("application_functionality", functionality);
+
+        if(message.contains("Currently in database you got")){
+            List<Task> tasks = dbService.getAllTasks();
+            if(tasks.size() > 0){
+                context.setVariable("tasks", tasks);
+                context.setVariable("is_display_tasks", true);
+            }
+        } else{
+            context.setVariable("is_display_tasks", false);
+        }
+
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
     private String createMessagePreview(String message){
